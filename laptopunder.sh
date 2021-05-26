66451dc7c75bff6d51a194121182b2a51acc709f
Edvard <elsberg1995@protonmail.com>
2021-05-26 09:46:20 +0200

    :zap:

---
+++ laptopunder.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output eDP --primary --mode 1920x1080 --pos 1920x1080 \
+--output DisplayPort-2 --mode 1920x1080 --pos 0x0 \
+--output DisplayPort-4 --mode 1920x1080 --pos 1920x0
